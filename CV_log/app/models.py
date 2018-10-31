a42857ea27f62a5abefb48d15d431040c6baab36
Hikaru Koizumi <aoxora333@gmail.com>
2018-11-01 02:31:01 +0900

    no message

--- CV_log/app/models.py
+++ CV_log/app/models.py
@@ -2,8 +2,12 @@
 from djongo import models
 
 class Example(models.Model):
-name = models.CharField(max_length=255, blank=False)
-value = models.FloatField()
-date_created = models.DateTimeField(auto_now_add=True)
-date_modified = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255, blank=False)
+    value = models.FloatField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
 
+    objects = models.DjongoManager()
+
+    def __str__(self):
+        return '%s' % self.name
---
+++ runmongodb.bat
@@ -0,0 +1,2 @@
+conda activate MongoDB
+mongod --install --config C:\Xserver\CV_logger_App\mongodb\mongodb.conf
